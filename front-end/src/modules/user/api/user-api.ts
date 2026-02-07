824b535b07a32488dd3d82d99b2072a8ac20b2fc
ashish-commit987 <rajputomsanskar@gmail.com>
2026-02-08 01:18:45 +0530

    Update user-api.ts

--- front-end/src/modules/user/api/user-api.ts
+++ front-end/src/modules/user/api/user-api.ts
@@ -1,12 +1,9 @@
-import axios from 'axios';
+import { apiClient } from '@/config/axios.config';
 
-const API_BASE_URL=import.meta.env.VITE_API_URL;
-axios.defaults.baseURL=API_BASE_URL;
-
-export const doRegister=(userData:unknown)=>{
-    return axios.post('register',userData);
+export const doRegister = (userData: unknown) => {
+    return apiClient.post('/api/v1/user/register', userData);
 }
 
-export const doLogin=(userData:unknown)=>{
-    return axios.post('login',userData);
+export const doLogin = (userData: unknown) => {
+    return apiClient.post('/api/v1/user/login', userData);
 }
